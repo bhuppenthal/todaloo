36a3e425132f2b19cfefae7bab4f149d18d709f7
getekj <joannagetek@gmail.com>
2022-10-07 13:42:25 -0400

    a mess

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import { GoogleMap, useLoadScript }from '@react-google-maps/api';
-
+import { useEffect, useState } from 'react';
+import { GoogleMap, useLoadScript, Marker }from '@react-google-maps/api';
+//import BathroomMarker from '../components/BathroomMarker';
 
 function Map () {
+
+  const [bathrooms, setBathrooms] = useState([]);
+
+  // Retrieve all the bathrooms in db
+  const loadBathrooms = async () => {
+      console.log("Entered load bathrooms")
+      const response = await fetch('http://localhost:3000/bathroom', { method: 'GET'});
+      const bathrooms = await response.json();
+      setBathrooms(bathrooms);
+      console.log(bathrooms.map(bathroom => bathroom.position));
+  }
+
+  //Load the bathrooms, used when the component is first mounted
+  useEffect(() => {
+      loadBathrooms();
+  }, []);
+
 
     const containerStyle = {
         height: "100vh",
@@ -22,6 +40,7 @@ function Map () {
     if (loadError) return "Error"; 
     if (!isLoaded) return "Loading...";
 
+    console.log(`HI you are in map.js here are the BRs: ${bathrooms}`)
     return (
         <>
         <h2>Hello i am a map</h2>
@@ -29,7 +48,14 @@ function Map () {
         <GoogleMap
             mapContainerStyle={containerStyle}
             center={center}
-            zoom={15}>
+            zoom={15}
+        >
+            {/* <BathroomMarker bathrooms={bathrooms}/> */}
+            {/* {bathrooms.map((bathroom) => (
+            <Marker 
+                position = {{lat: bathroom.position.lat, lng: bathroom.position.lng}}/>
+            ))}; */}
+            <Marker position = {{lat: 44.567132, lng: -123.272541}} />
         </GoogleMap>
         </>
     )
--- frontend/src/pages/MapView.js
+++ frontend/src/pages/MapView.js
@@ -3,29 +3,30 @@ import { useState, useEffect } from 'react';
 
 import Map from "../components/Map"
 
+
 function MapView ({setBathroomLatLng}) {
 
-    const [bathrooms, setBathrooms] = useState([]);
+    // const [bathrooms, setBathrooms] = useState([]);
 
-    // Retrieve all the bathrooms in db
-    const loadBathrooms = async () => {
-        console.log("Entered load bathrooms")
-        const response = await fetch('http://localhost:3000/bathroom', { method: 'GET'});
-        const bathrooms = await response.json();
-        setBathrooms(bathrooms);
-    }
+    // // Retrieve all the bathrooms in db
+    // const loadBathrooms = async () => {
+    //     console.log("Entered load bathrooms")
+    //     const response = await fetch('http://localhost:3000/bathroom', { method: 'GET'});
+    //     const bathrooms = await response.json();
+    //     setBathrooms(bathrooms);
+    //     console.log(bathrooms.map(bathroom => bathroom.position));
+    // }
 
-    //Load the bathrooms, used when the component is first mounted
-    useEffect(() => {
-        loadBathrooms();
-    }, []);
+    // //Load the bathrooms, used when the component is first mounted
+    // useEffect(() => {
+    //     loadBathrooms();
+    // }, []);
 
-    console.log(`Here are bathrooms positions from get req: ${bathrooms.position}`)
 
     return (
         <>
         <h1>MAPVIEW WORKS</h1>
-        <Map />
+        <Map/>
         </>
     );
 };

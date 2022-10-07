477dd238671d9da0595f9f27e50dedc7f7303027
getekj <joannagetek@gmail.com>
2022-10-07 00:21:49 -0400

    router working :')

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,36 @@
-import logo from './logo.svg';
+// importing dependencies
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import MapView from './pages/MapView';
+import CreateBathroom from './pages/CreateBathroom';
+
+// importing styles
 import './App.css';
 
-//joanna trying to commit
 
 function App() {
+
+  const [latLngToAdd, setLatLngToAdd] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    <h1>To Da Loo</h1>
+
+    <Router>
+      <Routes>
+      
+        <Route path="/" element={<MapView setLatLngToAdd={setLatLngToAdd}/>}> 
+        </Route>
+
+        <Route path= "/create-bathroom" element={<CreateBathroom latLngToAdd={latLngToAdd}/>}>
+        </Route>
+
+      </Routes>
+    </Router>
+
+  </>
   );
 }
 
---
+++ frontend/src/pages/MapView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MapView () {
+    return (
+        <>
+        <h1>MAPVIEW WORKS</h1>
+        </>
+    );
+};
+
+export default MapView;

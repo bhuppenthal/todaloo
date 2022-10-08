12d0f63422fbb32041d4a7c9473203ec051a6cdd
getekj <joannagetek@gmail.com>
2022-10-08 00:18:39 -0400

    new user page

--- frontend/src/pages/NewUserPage.js
+++ frontend/src/pages/NewUserPage.js
@@ -1,11 +1,63 @@
 import React from 'react';
 
 function NewUserRegister () {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submitNewUser = async (e) => {
+        const newUser = {
+            username: username,
+            password: password
+        };
+    
+
+    console.log(newUser)
+
+    const response = await fetch('/register', {
+        method: 'POST',
+        body: JSON.stringify(newUser),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (response.status === 201) {
+        alert("Login successful!");
+    } else {
+        alert(`Failed to login, status code = ${response.status}`)
+    }
+    };
+
     return (
-        <>
-        <p>testing new user page</p>
-        </>
-    )
+        <div className="register-title"> 
+         
+         <form onSubmit={(e) => {e.preventDefault();}}>
+            <label>Enter Username</label>
+            <input required
+                type="text"
+                placeholder="username"
+                value={username}
+                id="username"
+                onChange={(e) => setUsername(e.target.value)} 
+            />
+            <label>Enter Password </label>
+            <input required
+                type="text"
+                placeholder="password"
+                value={password}
+                id="password"
+                onChange={(e) => setPassword(e.target.value)} 
+            />
+            <button
+                type="submit"
+                className="button"
+                onClick={submitNewUser}>
+                Register
+            </button>
+         </form>
+        </div>
+    );
 };
 
 export default NewUserRegister;
